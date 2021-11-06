da9d04b8ff53a3f85527fe03d0a6186b8b72ccef
AllenJoeG <allen.joeg@gmail.com>
2021-11-06 11:57:21 -0500

    Update 0.3 submitButton(), renderTableToDOM(), addNewEmployeeToArray(), skeleton of calculate()

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ $(document).ready(readyFunction);
 
 function readyFunction(){
   console.log('jQuery.js Battlecruiser Operational')
-  $('#submitButton)').on('click', submitButton);
+  $('#submitButton').on('click', submitButton);
 
 };
 
@@ -13,11 +13,11 @@ const employeeArray = [];
 // Required Feature - creates new employee object ()
 function addNewEmployeeToArray(first, last, id, title, salary){
   let newEmployeeObject = {
-    firstName: ,
-    lastName: ,
-    idNumber: ,
-    jobTitle: ,
-    salary:
+    firstName: first,
+    lastName: last,
+    idNumber: id,
+    jobTitle: title,
+    salary: salary,
   }
 
   employeeArray.push(newEmployeeObject);
@@ -25,20 +25,58 @@ function addNewEmployeeToArray(first, last, id, title, salary){
 
 // push user values to new employee object ()
 function submitButton(){
-  //save the elements we'll interact with to shorthand
+  //get Input values
+  let firstNameIn = $('#firstNameInput').val();
+  let lastNameIn = $('#lastNameInput').val();
+  let idNumIn = $('#idNumberInput').val();
+  let titleIn = $('#jobTitleInput').val();
+  let salaryIn = $('#salaryInput').val();
 
+  addNewEmployeeToArray(firstNameIn, lastNameIn, idNumIn, titleIn, salaryIn);
+  console.log(employeeArray);
 
+  renderTableToDOM(employeeArray);
+
+  calculateValue(employeeArray);
+
+  //clear input values
+  $('#firstNameInput').val('');
+  $('#lastNameInput').val('');
+  $('#idNumberInput').val('');
+  $('#jobTitleInput').val('');
+  $('#salaryInput').val('');
 };
 
 // render [] to '#tableDiv' on DOM ()
-function renderUpdateDOM(){
+function renderTableToDOM(array){
+  //target relevant table Elements 
+  let tableBody = $('#tableBody');
+  //clear the <tbody> to redraw
+  tableBody.empty();
+
+  for (let employee of array) {
+    let newTableRow = `
+      <tr id="${employee.idNumber}">
+        <td>${employee.firstName}</td>
+        <td>${employee.lastName}</td>
+        <td>${employee.idNumber}</td>
+        <td>${employee.jobTitle}</td>
+        <td>${employee.salary}</td>
+      </tr>
+      `
+      console.log(newTableRow);
+      tableBody.append(newTableRow);
+  }
 };
 
 // calculate cumulative salary and render to DOM ()
 function calculateValue(array){
-
-}
-
-//
+  let salaryTotal = 0;
+  for (let employee in array){
+    salaryTotal += employee.salary
+  }
+  //add total to salary total output
+  $('#salaryTotal').text(salaryTotal);
+};
 
 // Delete ()
