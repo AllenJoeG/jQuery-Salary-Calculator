3ef2fc5dc9e4835cf7a3821058244c1adb24ea8f
AllenJoeG <allen.joeg@gmail.com>
2021-11-06 11:03:53 -0500

    Update 0.2 pseudo code and function structure to client.js

--- client.js
+++ client.js
@@ -1,6 +1,44 @@
-console.log('jscript.js battlecruiser operational')
+console.log('jscript.js SCV ready')
 $(document).ready(readyFunction);
 
 function readyFunction(){
-console.log('jQuery.js battlecruiser operational')
+  console.log('jQuery.js Battlecruiser Operational')
+  $('#submitButton)').on('click', submitButton);
+
 };
+
+// array of employee objects []
+const employeeArray = [];
+
+// Required Feature - creates new employee object ()
+function addNewEmployeeToArray(first, last, id, title, salary){
+  let newEmployeeObject = {
+    firstName: ,
+    lastName: ,
+    idNumber: ,
+    jobTitle: ,
+    salary:
+  }
+
+  employeeArray.push(newEmployeeObject);
+}
+
+// push user values to new employee object ()
+function submitButton(){
+  //save the elements we'll interact with to shorthand
+
+
+};
+
+// render [] to '#tableDiv' on DOM ()
+function renderUpdateDOM(){
+};
+
+// calculate cumulative salary and render to DOM ()
+function calculateValue(array){
+
+}
+
+//
+
+// Delete ()
