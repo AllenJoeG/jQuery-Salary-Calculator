2bbe111a9a85d8bb51bd278262893a11ae0eded1
AllenJoeG <allen.joeg@gmail.com>
2021-11-06 12:22:04 -0500

    Update 0.4 calculateMonthly() complete. TODO- delete employee and row ()

--- client.js
+++ client.js
@@ -4,11 +4,33 @@ $(document).ready(readyFunction);
 function readyFunction(){
   console.log('jQuery.js Battlecruiser Operational')
   $('#submitButton').on('click', submitButton);
-
 };
 
 // array of employee objects []
 const employeeArray = [];
+
+// push user values to new employee object ()
+function submitButton(){
+  //get Input values
+  let firstNameIn = $('#firstNameInput').val();
+  let lastNameIn = $('#lastNameInput').val();
+  let idNumIn = $('#idNumberInput').val();
+  let titleIn = $('#jobTitleInput').val();
+  let salaryIn = $('#salaryInput').val();
+
+  addNewEmployeeToArray(firstNameIn, lastNameIn, Number(idNumIn), titleIn, Number(salaryIn));
+
+  renderTableToDOM(employeeArray);
+
+  calculateMonthly(employeeArray);
+
+  //clear input values
+  $('#firstNameInput').val('');
+  $('#lastNameInput').val('');
+  $('#idNumberInput').val('');
+  $('#jobTitleInput').val('');
+  $('#salaryInput').val('');
+};
 
 // Required Feature - creates new employee object ()
 function addNewEmployeeToArray(first, last, id, title, salary){
@@ -23,31 +45,7 @@ function addNewEmployeeToArray(first, last, id, title, salary){
   employeeArray.push(newEmployeeObject);
 }
 
-// push user values to new employee object ()
-function submitButton(){
-  //get Input values
-  let firstNameIn = $('#firstNameInput').val();
-  let lastNameIn = $('#lastNameInput').val();
-  let idNumIn = $('#idNumberInput').val();
-  let titleIn = $('#jobTitleInput').val();
-  let salaryIn = $('#salaryInput').val();
-
-  addNewEmployeeToArray(firstNameIn, lastNameIn, idNumIn, titleIn, salaryIn);
-  console.log(employeeArray);
-
-  renderTableToDOM(employeeArray);
-
-  calculateValue(employeeArray);
-
-  //clear input values
-  $('#firstNameInput').val('');
-  $('#lastNameInput').val('');
-  $('#idNumberInput').val('');
-  $('#jobTitleInput').val('');
-  $('#salaryInput').val('');
-};
-
-// render [] to '#tableDiv' on DOM ()
+// Required Feature - render [] to '#tableDiv' on DOM ()
 function renderTableToDOM(array){
   //target relevant table Elements 
   let tableBody = $('#tableBody');
@@ -69,14 +67,28 @@ function renderTableToDOM(array){
   }
 };
 
-// calculate cumulative salary and render to DOM ()
-function calculateValue(array){
-  let salaryTotal = 0;
-  for (let employee in array){
-    salaryTotal += employee.salary
-  }
+// Required Feature - calculate cumulative salary and render to DOM ()
+function calculateMonthly(array){
+  let annualSalaryTotal = 0;
+  let monthlySalaryExpense = 0;
+  const monthlySalaryMax = 20000;
+
+  console.log(array);
+  for (let employee of array){
+    console.log(employee.salary);
+    annualSalaryTotal = employee.salary;
+  };
+
+  console.log(annualSalaryTotal)
+
+  monthlySalaryExpense = Math.floor(annualSalaryTotal / 12);
+  console.log(monthlySalaryExpense);
+
   //add total to salary total output
-  $('#salaryTotal').text(salaryTotal);
+  $('#monthlyTotal').text(`${monthlySalaryExpense}`);
+
+
+
 };
 
 // Delete ()
